51c2706f62759f0f6ecfc13826ddbbcd105de6fd
Saboor <abdus.ansari@republicworld.com>
2025-02-13 16:24:09 +0530

    Frontend setup done

---
+++ frontend/src/index.js
@@ -0,0 +1,13 @@
+import App from "./App";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
+
+ReactDOM.render(
+  <ChakraProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ChakraProvider>,
+  document.querySelector("#root")
+);
