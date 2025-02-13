d474853eaaed298732c8f72740ebb9be0ab50357
Saboor <abdus.ansari@republicworld.com>
2025-02-13 16:37:41 +0530

    Added Mongo Db config , added sample users and added error middlewares

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,17 @@
 import express from "express";
 import path from "path";
 import dotenv from "dotenv";
+import connectDb from "./config/db.js";
+import { errorHandler, notFound } from "./middlewares/errorMiddleware.js";
 
 dotenv.config();
+connectDb();
 
 const app = express();
 
 app.use(express.json());
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () =>
